69bd0d1f642c609825c3f05fe4e12489870fe2c8
Drew Thompson <drewallynthompson@gmail.com>
2021-01-05 10:16:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function updateDriverWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateDriverWithKeyAndValue(object, key, value){
   object[key] = value;
-  return object
 }
 
 function deleteFromDriverByKey(object, key){
