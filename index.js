eeba08ba202000b1e7006b6a5d69ed5aabbb3043
Drew Thompson <drewallynthompson@gmail.com>
2021-01-05 10:14:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let driver = {};
 
 function updateDriverWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {key: value});
+  return Object.assign({}, object, {`${key}`: value});
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(object, key, value){
